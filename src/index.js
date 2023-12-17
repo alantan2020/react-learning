bc236fadece1ba55ced858a23a07eb8e88af9e9d
alantan2020 <582704597@qq.com>
2023-12-18 01:54:12 +0800

    路由配置

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import sum from "@/text";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 const total = sum(1, 5);
 console.log(total);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={router} />);

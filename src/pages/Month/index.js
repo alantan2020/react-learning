62d8cecff57034022ec5c1f79097271099932327
alantan2020 <582704597@qq.com>
2023-12-31 00:44:36 +0800

    月度帐单中的切换弹框功能

--- src/pages/Month/index.js
+++ src/pages/Month/index.js
@@ -1,7 +1,16 @@
 import { NavBar, DatePicker } from "antd-mobile";
+import React, { useState } from "react";
 import "./index.scss";
+import classNames from "classnames";
 
 const Month = () => {
+  // 控制弹框的打开和关闭
+  const [dateVisible, setDateVisible] = useState(false);
+
+  const onConfirm = () => {
+    setDateVisible(false);
+    //其他逻辑
+  };
   return (
     <div className="monthlyBill">
       <NavBar className="nav" backArrow={false}>
@@ -10,9 +19,12 @@ const Month = () => {
       <div className="content">
         <div className="header">
           {/* 时间切换区域 */}
-          <div className="date">
+          <div className="date" onClick={() => setDateVisible(true)}>
             <span className="text">2023 | 3月账单</span>
-            <span className="arrow expand"></span>
+            {/* 根据当前弹框打开的状态控制expand类名的是否存在 */}
+            <span
+              className={classNames("arrow", dateVisible && "expand")}
+            ></span>
           </div>
           {/* 统计区域 */}
           <div className="twoLineOverview">
@@ -34,7 +46,10 @@ const Month = () => {
             className="kaDate"
             title="记账日期"
             precision="month"
-            visible={false}
+            visible={dateVisible}
+            onCancel={() => setDateVisible(false)}
+            onConfirm={onConfirm}
+            onClose={() => setDateVisible(false)}
             max={new Date()}
           />
         </div>
@@ -63,8 +78,8 @@ export default Month;
 //   }, [billList])
 //   console.log(monthGroup)
 
-//   // 控制弹框的打开和关闭
-//   const [dateVisible, setDateVisible] = useState(false)
+// // 控制弹框的打开和关闭
+// const [dateVisible, setDateVisible] = useState(false)
 
 //   // 控制时间显示
 //   const [currentDate, setCurrentDate] = useState(() => {

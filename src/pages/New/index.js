b7228ac8ab2c6b01f83bf1d888db7182b8daf599
alantan2020 <582704597@qq.com>
2024-01-03 02:24:28 +0800

    记帐功能中的支出和收入状态的切换

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -4,9 +4,12 @@ import "./index.scss";
 import classNames from "classnames";
 import { billListData } from "@/contants";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const New = () => {
   const navigate = useNavigate();
+  //准备一个控制收入支出的状态
+  const [billType, setBillType] = useState("pay"); //pay-支出  income-收入
   return (
     <div className="keepAccounts">
       <NavBar className="nav" onBack={() => navigate(-1)}>
@@ -15,10 +18,18 @@ const New = () => {
 
       <div className="header">
         <div className="kaType">
-          <Button shape="rounded" className={classNames("selected")}>
+          <Button
+            shape="rounded"
+            className={classNames(billType === "pay" ? "selected" : "")}
+            onClick={() => setBillType("pay")}
+          >
             支出
           </Button>
-          <Button className={classNames("")} shape="rounded">
+          <Button
+            className={classNames(billType === "income" ? "selected" : "")}
+            shape="rounded"
+            onClick={() => setBillType("income")}
+          >
             收入
           </Button>
         </div>
@@ -43,7 +54,8 @@ const New = () => {
       </div>
 
       <div className="kaTypeList">
-        {billListData["pay"].map((item) => {
+        {/* 数据区域 */}
+        {billListData[billType].map((item) => {
           return (
             <div className="kaType" key={item.type}>
               <div className="title">{item.name}</div>

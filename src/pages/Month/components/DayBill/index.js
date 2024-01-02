18e18c9a0598e632bd6686ceb56efb2e3a3d3375
alantan2020 <582704597@qq.com>
2024-01-03 01:55:00 +0800

    帐单类型图标组件的封装

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -2,6 +2,7 @@ import classNames from "classnames";
 import "./index.scss";
 import { useMemo, useState } from "react";
 import { billTypeToName } from "@/contants/index";
+import Icon from "@/conponents";
 
 const DailyBill = ({ date, billList }) => {
   const dayResult = useMemo(() => {
@@ -53,6 +54,8 @@ const DailyBill = ({ date, billList }) => {
         {billList.map((item) => {
           return (
             <div className="bill" key={item.id}>
+              {/* 图标 */}
+              <Icon type={item.useFor} />
               <div className="detail">
                 <div className="billType">{billTypeToName[item.useFor]}</div>
               </div>
